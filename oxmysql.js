f252c94413fa03738049fc597489d1b2f84724ae
Linden <65407488+thelindat@users.noreply.github.com>
2021-08-29 09:16:39 +1000

    Refactor functions; change pool options

--- oxmysql.js
+++ oxmysql.js
@@ -6,52 +6,52 @@ const pool = createPool({
     user: 'root',
     password: 'password',
     database: 'database',
-    charset: 'utf8mb4',
-    multipleStatements: true,
+    charset: 'utf8mb4_unicode_ci',
+    multipleStatements: false,
     namedPlaceholders: true,
     waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
 const execute = async (query, parameters) => {
+    ScheduleResourceTick(GetCurrentResourceName());
     try {
         console.time(query);
         const [result] = await pool.execute(query, parameters);
         console.timeEnd(query);
-
         return result;
     } catch (error) {
-        console.error(error.message);
-
-        return;
+        return console.error(error.message);
     }
 }
 
-global.exports("execute", (query, parameters = [], callback = () => { }) => {
-    ScheduleResourceTick(GetCurrentResourceName());
-    execute(query, parameters)
-        .then(result => callback(result && result.affectedRows));
+global.exports("execute", (query, parameters, callback = () => { }) => {
+    execute(query, parameters).then(result =>
+        callback(result && result.affectedRows)
+    );
 });
 
-global.exports("fetch", (query, parameters = [], callback = () => { }) => {
-    ScheduleResourceTick(GetCurrentResourceName());
-    execute(query, parameters)
-        .then(result => callback(result));
+global.exports("fetch", (query, parameters, callback = () => { }) => {
+    execute(query, parameters).then(result =>
+        callback(result)
+    );
 });
 
-global.exports("single", (query, parameters = [], callback = () => { }) => {
-    ScheduleResourceTick(GetCurrentResourceName());
-    execute(query, parameters)
-        .then(result => callback(result && result[0]));
+global.exports("single", (query, parameters, callback = () => { }) => {
+    execute(query, parameters).then(result =>
+        callback(result && result[0])
+    );
 });
 
-global.exports("scalar", (query, parameters = [], callback = () => { }) => {
-    ScheduleResourceTick(GetCurrentResourceName());
-    execute(query, parameters)
-        .then(result => callback(result && result[0] && Object.values(result[0])[0]));
+global.exports("scalar", (query, parameters, callback = () => { }) => {
+    execute(query, parameters).then(result =>
+        callback(result && result[0] && Object.values(result[0])[0])
+    );
 });
 
-global.exports("insert", (query, parameters = [], callback = () => { }) => {
-    ScheduleResourceTick(GetCurrentResourceName());
-    execute(query, parameters)
-        .then(result => callback(result && result.insertId));
+global.exports("insert", (query, parameters, callback = () => { }) => {
+    execute(query, parameters).then(result =>
+        callback(result && result.insertId)
+    );
 });
