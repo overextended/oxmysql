c5f150e0032fbbd4bc062febdbaae1dd016104a6
Linden <65407488+thelindat@users.noreply.github.com>
2021-09-19 04:25:42 +1000

    improvement: Only display warnings if callback is defined
    - Non-function callbacks will display a warning.
    - This makes the third argument optional for async queries.

--- src/index.js
+++ src/index.js
@@ -10,39 +10,41 @@ setImmediate(async () => {
   }
 });
 
-const safeCallback = (callback, result, error) => {
-  if (typeof callback === 'function') return callback(result || false);
-  else return console.log(`^3[WARNING] ${error[0]} executed a query, but no callback function was defined!\n        ^3 ${error[1]}^0`);
+const safeCallback = (callback, result, resource, query) => {
+  if (typeof callback === 'function')
+    return callback(result || false);
+  else if (callback)
+    return console.log(`^3[WARNING] ${resource} executed a query, but no callback function was defined!\n        ^3 ${query}^0`);
 };
 
 global.exports('execute', (query, parameters, cb, resource = GetInvokingResource()) => {
   execute(query, parameters, resource).then((result) =>
-    safeCallback(cb, result, !cb && [resource, query]));
+    safeCallback(cb, result, resource, query));
 });
 
 global.exports('insert', (query, parameters, cb, resource = GetInvokingResource()) => {
   execute(query, parameters, resource).then((result) =>
-    safeCallback(cb, result && result.insertId, !cb && [resource, query]));
+    safeCallback(cb, result && result.insertId, resource, query));
 });
 
 global.exports('update', (query, parameters, cb, resource = GetInvokingResource()) => {
-  execute(query, parameters, resource).then((result) =>
-    safeCallback(cb, result && result.affectedRows, !cb && [resource, query]));
+  execute(query, parameters, resource).then((result) => 
+    safeCallback(cb, result && result.affectedRows, resource, query));
 });
 
 global.exports('fetch', (query, parameters, cb, resource = GetInvokingResource()) => {
   execute(query, parameters, resource).then((result) =>
-    safeCallback(cb, result, !cb && [resource, query]));
+    safeCallback(cb, result, resource, query));
 });
 
 global.exports('single', (query, parameters, cb, resource = GetInvokingResource()) => {
   execute(query, parameters, resource).then((result) =>
-    safeCallback(cb, result && result[0], !cb && [resource, query]));
+    safeCallback(cb, result && result[0], resource, query));
 });
 
 global.exports('scalar', (query, parameters, cb, resource = GetInvokingResource()) => {
   execute(query, parameters, resource).then((result) =>
-    safeCallback(cb, result && result[0] && Object.values(result[0])[0], !cb && [resource, query]));
+    safeCallback(cb, result && result[0] && Object.values(result[0])[0], resource, query));
 });
 
 if (!GetResourceMetadata(GetCurrentResourceName(), 'server_script', 1)) {
