2c623af6e235f125cf0c135afb6c11b3a404753b
Linden <65407488+thelindat@users.noreply.github.com>
2021-10-19 08:49:13 +1100

    feat: Add an export to call prepared queries
    - Utilises pool.execute (rather than query) to actually prepare parameters through mysql.
    
    This is intended for appropriately built queries utilising  placeholders and parameters, and should only be used on frequently repeated queries.

--- src/execute.js
+++ src/execute.js
@@ -1,13 +1,11 @@
 import { pool } from './pool';
 import { parseParameters } from './parser';
 import { slowQueryWarning, debug, resourceName } from './config';
+import { FormatError } from './errors';
 
 const execute = async (query, parameters, resource) => {
-  ScheduleResourceTick(resourceName);
   try {
-
     [query, parameters] = parseParameters(query, parameters);
-
     const [rows, _, executionTime] = await pool.query(query, parameters);
 
     if (executionTime >= slowQueryWarning || debug)
@@ -16,6 +14,7 @@ const execute = async (query, parameters, resource) => {
         ${query} ${JSON.stringify(parameters)}^0`
       );
 
+    ScheduleResourceTick(resourceName);
     return rows;
   } catch (error) {
     console.log(
@@ -27,4 +26,47 @@ const execute = async (query, parameters, resource) => {
   }
 };
 
-export { execute };
+const queryType = (query) => {
+  switch (query.replace(/\s.*/, '')) {
+    case 'SELECT':
+      return 1;
+    case 'INSERT':
+      return 2;
+    case 'UPDATE':
+      return 3;
+    case 'DELETE':
+      return 3;
+    default:
+      return false;
+  }
+};
+
+const preparedStatement = async (query, parameters, resource) => {
+  try {
+    if (!Array.isArray(parameters))
+      throw new FormatError(`Placeholders were defined, but query received no parameters!`, query);
+
+    if (typeof query !== 'string') throw new FormatError(`Prepared statements must utilise a single query`);
+
+    const type = queryType(query);
+    if (!type) throw new FormatError(`Prepared statements only accept SELECT, INSERT, UPDATE, and DELETE methods!`);
+
+    const results = [];
+    for (let i = 0; i < parameters.length; i++) {
+      const [rows] = await pool.execute(query, parameters[i]);
+      results[i] = rows && (type === 1 ? rows : type === 2 ? rows.insertId : type === 3 ? rows.affectedRows : [rows]);
+    }
+
+    ScheduleResourceTick(resourceName);
+    return results.length === 1? (type === 1 ? results[0][0] : results[0]) : results;
+  } catch (error) {
+    console.log(
+      `^1[ERROR] ${resource} was unable to execute a query!
+        ${error.message}
+        ${error.sql || `${query} ${JSON.stringify(parameters)}`}^0`
+    );
+    debug && console.trace(error);
+  }
+};
+
+export { execute, preparedStatement };
