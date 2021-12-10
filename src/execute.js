cd2e73953fb0916e543df908ac64008b4473264c
Linden <65407488+thelindat@users.noreply.github.com>
2021-12-10 11:17:02 +1100

    test: re-enable slow query warnings

--- src/execute.js
+++ src/execute.js
@@ -8,15 +8,15 @@ const execute = async (query, parameters, resource) => {
     [query, parameters] = parseParameters(query, parameters);
 
     ScheduleResourceTick(resourceName);
-    // const startTime = process.hrtime();
+    const startTime = process.hrtime();
     const [rows] = await pool.query(query, parameters);
-    // const executionTime = process.hrtime(startTime)[1] / 1000000; // nanosecond to millisecond
+    const executionTime = process.hrtime(startTime)[1] / 1000000; // nanosecond to millisecond
 
-    // if (executionTime >= slowQueryWarning || debug)
-    //   console.log(
-    //     `^3[${debug ? 'DEBUG' : 'WARNING'}] ${resource} took ${executionTime}ms to execute a query!
-    //     ${query} ${JSON.stringify(parameters)}^0`
-    //   );
+    if (executionTime >= slowQueryWarning || debug)
+      console.log(
+        `^3[${debug ? 'DEBUG' : 'WARNING'}] ${resource} took ${executionTime}ms to execute a query!
+        ${query} ${JSON.stringify(parameters)}^0`
+      );
 
     return rows;
   } catch (error) {
@@ -58,21 +58,21 @@ const preparedStatement = async (query, parameters, resource) => {
 
     const results = [];
     let queryCount = parameters.length;
-    // const startTime = process.hrtime();
+    const startTime = process.hrtime();
 
     for (let i = 0; i < queryCount; i++) {
       const [rows] = await connection.execute(query, parameters[i]);
       results[i] = rows && (type === 3 ? rows.affectedRows : type === 2 ? rows.insertId : rows);
     }
 
-    // const executionTime = process.hrtime(startTime)[1] / 1000000; // nanosecond to millisecond
-    // if (executionTime >= slowQueryWarning || debug)
-    //   console.log(
-    //     `^3[${debug ? 'DEBUG' : 'WARNING'}] ${resource} took ${executionTime}ms to execute ${
-    //       queryCount > 1 ? queryCount + ' queries' : 'a query'
-    //     }!
-    //     ${query} ${JSON.stringify(parameters)}^0`
-    //   );
+    const executionTime = process.hrtime(startTime)[1] / 1000000; // nanosecond to millisecond
+    if (executionTime >= slowQueryWarning || debug)
+      console.log(
+        `^3[${debug ? 'DEBUG' : 'WARNING'}] ${resource} took ${executionTime}ms to execute ${
+          queryCount > 1 ? queryCount + ' queries' : 'a query'
+        }!
+        ${query} ${JSON.stringify(parameters)}^0`
+      );
 
     if (results.length === 1) {
       if (type === 1) {
