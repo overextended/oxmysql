d1a5a9e249e038fb46b7d7bc58f598e16248d5d3
Luke <sabolukas03@gmail.com>
2021-12-31 15:10:10 +0100

    tweak(nui): Alignment

--- ui/src/components/NavBars/TopBar.tsx
+++ ui/src/components/NavBars/TopBar.tsx
@@ -3,7 +3,7 @@ import { Box } from '@chakra-ui/react';
 const TopBar: React.FC = () => {
   return (
     <Box w="100%" h="5%">
-      <Box p="1.2vh" fontSize="2.2vh">
+      <Box paddingLeft="1.2vh" fontSize="2.2vh">
         Resources
       </Box>
     </Box>
