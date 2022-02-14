86e51e79ef369d927a39fbb3ddf583571396eae4
Linden <65407488+thelindat@users.noreply.github.com>
2022-02-14 22:25:59 +1100

    refactor(update): Use GitHub releases API
    Compare current version to latest release.

--- src/update/index.ts
+++ src/update/index.ts
@@ -2,16 +2,17 @@ import fetch from 'node-fetch';
 import { resourceName } from '../config';
 
 setTimeout(async () => {
-  const response = await fetch(`https://raw.githubusercontent.com/overextended/oxmysql/main/lua/fxmanifest.lua`);
+  const response = await fetch(`https://api.github.com/repos/overextended/oxmysql/releases/latest`);
 
   if (response.status !== 200) return;
 
-  const manifest = await response.text();
+  const release = (await response.json()) as any;
+  if (release.prerelease) return;
 
   const currentVersion = GetResourceMetadata(resourceName, 'version', 0).match(/(\d)\.(\d+)\.(\d+)/);
   if (!currentVersion) return;
 
-  const latestVersion = manifest.match(/(\d)\.(\d+)\.(\d+)/);
+  const latestVersion = release.tag_name.match(/(\d)\.(\d+)\.(\d+)/);
   if (!latestVersion) return;
 
   if (currentVersion[0] === latestVersion[0]) return;
@@ -24,6 +25,6 @@ setTimeout(async () => {
       : 'a major update';
 
   console.log(
-    `^3There is ${updateMessage} available for oxmysql - please update to the latest release (current version: ${currentVersion[0]})\r\nhttps://github.com/overextended/oxmysql/releases/download/v${latestVersion[0]}/oxmysql-v${latestVersion[0]}.zip^0`
+    `^3There is ${updateMessage} available for oxmysql - please update to the latest release (current version: ${currentVersion[0]})\r\n${release.html_url}.zip^0`
   );
 }, 1000);
