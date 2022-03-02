cd62fdd904037b3551c55c562acd2b37f15eb080
Linden <65407488+thelindat@users.noreply.github.com>
2022-03-02 23:24:52 +1100

    refactor(utils/parseArguments): Reduce redundant assignments
    More in-line with 1.9.3 parser; reduced garbage and improved performance. Should handle JSON better.

--- src/utils/parseArguments.ts
+++ src/utils/parseArguments.ts
@@ -10,9 +10,10 @@ export const parseArguments = (
 ): [string, CFXParameters, CFXCallback | undefined] => {
   if (typeof query !== 'string') throw new Error(`Query expected a string but received ${typeof query} instead`);
 
-  const queryParams = query.match(/\?(?!\?)/g);
-
-  if ((typeof parameters === 'object' && !Array.isArray(parameters) && query.includes(':')) || query.includes('@')) {
+  if (
+    (query.includes(':') && parameters && typeof parameters === 'object' && !Array.isArray(parameters)) ||
+    query.includes('@')
+  ) {
     const placeholders = convertNamedPlaceholders(query, parameters);
     query = placeholders[0];
     parameters = placeholders[1];
@@ -20,27 +21,32 @@ export const parseArguments = (
 
   if (cb && typeof cb !== 'function') cb = undefined;
 
-  if (parameters && typeof parameters === 'function') cb = parameters;
-
-  if (parameters === null || parameters === undefined || typeof parameters === 'function') parameters = [];
+  if (parameters && typeof parameters === 'function') {
+    cb = parameters;
+    parameters = [];
+  } else if (parameters === null || parameters === undefined) parameters = [];
 
   if (!Array.isArray(parameters)) {
     let arr: unknown[] = [];
     Object.entries(parameters).forEach((entry) => (arr[parseInt(entry[0]) - 1] = entry[1]));
     parameters = arr;
-  } else if (queryParams !== null) {
-    if (parameters.length === 0) {
-      for (let i = 0; i < queryParams.length; i++) parameters[i] = null;
-      return [query, parameters, cb];
-    }
-    const diff = queryParams.length - parameters.length;
+  } else {
+    const queryParams = query.match(/\?(?!\?)/g);
 
-    if (diff > 0) {
-      for (let i = 0; i < diff; i++) parameters[queryParams.length + i] = null;
-    } else if (diff < 0) {
-      throw new Error(`${invokingResource} was unable to execute a query!
-          Expected ${queryParams.length} parameters, but received ${parameters.length}.
-          ${`${query} ${JSON.stringify(parameters)}`}`);
+    if (queryParams !== null) {
+      if (parameters.length === 0) {
+        for (let i = 0; i < queryParams.length; i++) parameters[i] = null;
+        return [query, parameters, cb];
+      }
+      const diff = queryParams.length - parameters.length;
+
+      if (diff > 0) {
+        for (let i = 0; i < diff; i++) parameters[queryParams.length + i] = null;
+      } else if (diff < 0) {
+        throw new Error(`${invokingResource} was unable to execute a query!
+        Expected ${queryParams.length} parameters, but received ${parameters.length}.
+        ${`${query} ${JSON.stringify(parameters)}`}`);
+      }
     }
   }
 
