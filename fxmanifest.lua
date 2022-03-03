4bbbe1efc2670963ae8e81ccb2b689d441935832
Linden <65407488+thelindat@users.noreply.github.com>
2022-03-03 19:51:00 +1100

    refactor(index): cease resistance against 0 iq fivem community
    Why did I think maintaining this type of resource would be a good idea?

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'common'
 
 name 'oxmysql'
 description 'Database wrapper for FiveM utilising node-mysql2 offering improved performance and security.'
-version '2.0.4'
+version '2.1.0'
 url 'https://github.com/overextended/oxmysql'
 author 'overextended'
 
--- src/index.ts
+++ src/index.ts
@@ -2,97 +2,114 @@ import { CFXCallback, CFXParameters, TransactionQuery } from './types';
 import { rawQuery } from './database/rawQuery';
 import { rawTransaction } from './database/rawTransaction';
 import { rawExecute } from './database/rawExecute';
-
-//TODO
 import('./update');
 
-global.exports(
-  'query',
-  (query: string, parameters: CFXParameters, cb: CFXCallback, invokingResource = GetInvokingResource()) => {
-    rawQuery(null, invokingResource, query, parameters, cb);
+const MySQL = {} as Record<string, Function>;
+
+MySQL.query = (query: string, parameters: CFXParameters, cb: CFXCallback, invokingResource = GetInvokingResource()) => {
+  rawQuery(null, invokingResource, query, parameters, cb);
+};
+
+MySQL.query_async = (query: string, parameters: CFXParameters, invokingResource = GetInvokingResource()) =>
+  rawQuery(null, invokingResource, query, parameters);
+
+MySQL.single = (
+  query: string,
+  parameters: CFXParameters,
+  cb: CFXCallback,
+  invokingResource = GetInvokingResource()
+) => {
+  rawQuery('single', invokingResource, query, parameters, cb);
+};
+
+MySQL.single_async = (query: string, parameters: CFXParameters, invokingResource = GetInvokingResource()) =>
+  rawQuery('single', invokingResource, query, parameters);
+
+MySQL.scalar = (
+  query: string,
+  parameters: CFXParameters,
+  cb: CFXCallback,
+  invokingResource = GetInvokingResource()
+) => {
+  rawQuery('scalar', invokingResource, query, parameters, cb);
+};
+
+MySQL.scalar_async = (query: string, parameters: CFXParameters, invokingResource = GetInvokingResource()) =>
+  rawQuery('scalar', invokingResource, query, parameters);
+
+MySQL.update = (
+  query: string,
+  parameters: CFXParameters,
+  cb: CFXCallback,
+  invokingResource = GetInvokingResource()
+) => {
+  rawQuery('update', invokingResource, query, parameters, cb);
+};
+
+MySQL.update_async = (query: string, parameters: CFXParameters, invokingResource = GetInvokingResource()) =>
+  rawQuery('update', invokingResource, query, parameters);
+
+MySQL.insert = (
+  query: string,
+  parameters: CFXParameters,
+  cb: CFXCallback,
+  invokingResource = GetInvokingResource()
+) => {
+  rawQuery('insert', invokingResource, query, parameters, cb);
+};
+
+MySQL.insert_async = (query: string, parameters: CFXParameters, invokingResource = GetInvokingResource()) =>
+  rawQuery('insert', invokingResource, query, parameters);
+
+MySQL.transaction = (
+  queries: TransactionQuery[] | string[],
+  parameters: CFXParameters,
+  cb: (result: boolean) => void,
+  invokingResource = GetInvokingResource()
+) => {
+  rawTransaction(invokingResource, queries, parameters, cb);
+};
+
+MySQL.transaction_async = (
+  queries: TransactionQuery[] | string[],
+  parameters: CFXParameters,
+  invokingResource = GetInvokingResource()
+) => rawTransaction(invokingResource, queries, parameters);
+
+MySQL.prepare = (
+  query: string,
+  parameters: CFXParameters | CFXParameters[],
+  cb: CFXCallback,
+  invokingResource = GetInvokingResource()
+) => {
+  rawExecute(invokingResource, query, parameters, cb);
+};
+
+MySQL.prepare_async = (
+  query: string,
+  parameters: CFXParameters | CFXParameters[],
+  invokingResource = GetInvokingResource()
+) => rawExecute(invokingResource, query, parameters);
+
+// Continue providing support for exports that were deprecated in v1.9.0 and removed in v2.0.0
+// Unfortunately, it's unrealistic to expect people to refer to documentation and update their
+// resources accordingly, especially when dealing with the special snowflakes on FiveM who only
+// care about squeezing as much money out of the community as possible.
+MySQL.execute = MySQL.query;
+MySQL.executeSync = MySQL.query_async;
+MySQL.fetch = MySQL.query;
+MySQL.fetchSync = MySQL.query_async;
+
+for (const key in MySQL) {
+  global.exports(key, MySQL[key]);
+
+  if (key.includes('_async')) {
+    const alias = `${key.slice(0, -6)}Sync`;
+    global.exports(
+      alias,
+      (query: string, parameters: CFXParameters | CFXParameters[], invokingResource = GetInvokingResource()) => {
+        return MySQL[key](query, parameters, invokingResource);
+      }
+    );
   }
-);
-
-global.exports('query_async', (query: string, parameters: CFXParameters, invokingResource = GetInvokingResource()) =>
-  rawQuery(null, invokingResource, query, parameters)
-);
-
-global.exports(
-  'single',
-  (query: string, parameters: CFXParameters, cb: CFXCallback, invokingResource = GetInvokingResource()) => {
-    rawQuery('single', invokingResource, query, parameters, cb);
-  }
-);
-
-global.exports('single_async', (query: string, parameters: CFXParameters, invokingResource = GetInvokingResource()) =>
-  rawQuery('single', invokingResource, query, parameters)
-);
-
-global.exports(
-  'scalar',
-  (query: string, parameters: CFXParameters, cb: CFXCallback, invokingResource = GetInvokingResource()) => {
-    rawQuery('scalar', invokingResource, query, parameters, cb);
-  }
-);
-
-global.exports('scalar_async', (query: string, parameters: CFXParameters, invokingResource = GetInvokingResource()) =>
-  rawQuery('scalar', invokingResource, query, parameters)
-);
-
-global.exports(
-  'update',
-  (query: string, parameters: CFXParameters, cb: CFXCallback, invokingResource = GetInvokingResource()) => {
-    rawQuery('update', invokingResource, query, parameters, cb);
-  }
-);
-
-global.exports('update_async', (query: string, parameters: CFXParameters, invokingResource = GetInvokingResource()) =>
-  rawQuery('update', invokingResource, query, parameters)
-);
-
-global.exports(
-  'insert',
-  (query: string, parameters: CFXParameters, cb: CFXCallback, invokingResource = GetInvokingResource()) => {
-    rawQuery('insert', invokingResource, query, parameters, cb);
-  }
-);
-
-global.exports('insert_async', (query: string, parameters: CFXParameters, invokingResource = GetInvokingResource()) =>
-  rawQuery('insert', invokingResource, query, parameters)
-);
-
-global.exports(
-  'transaction',
-  (
-    queries: TransactionQuery[] | string[],
-    parameters: CFXParameters,
-    cb: (result: boolean) => void,
-    invokingResource = GetInvokingResource()
-  ) => {
-    rawTransaction(invokingResource, queries, parameters, cb);
-  }
-);
-
-global.exports(
-  'transaction_async',
-  (queries: TransactionQuery[] | string[], parameters: CFXParameters, invokingResource = GetInvokingResource()) =>
-    rawTransaction(invokingResource, queries, parameters)
-);
-
-global.exports(
-  'prepare',
-  (
-    query: string,
-    parameters: CFXParameters | CFXParameters[],
-    cb: CFXCallback,
-    invokingResource = GetInvokingResource()
-  ) => {
-    rawExecute(invokingResource, query, parameters, cb);
-  }
-);
-
-global.exports(
-  'prepare_async',
-  (query: string, parameters: CFXParameters | CFXParameters[], invokingResource = GetInvokingResource()) =>
-    rawExecute(invokingResource, query, parameters)
-);
+}
