6c8934f906df4d7bb84398ee4da0cefcb38dd199
Linden <65407488+thelindat@users.noreply.github.com>
2022-03-27 08:37:11 +1100

    perf(execute): ignore results if cb is undefined
    Do not parse results when a callback argument was not received.
    Minor improvements to response parsing.

--- src/database/rawExecute.ts
+++ src/database/rawExecute.ts
@@ -19,39 +19,27 @@ export const rawExecute = async (
   await scheduleTick();
 
   const connection = await pool.promise().getConnection();
-  let response: QueryResponse;
+  const response = [] as RowDataPacket;
 
   try {
     const placeholders = query.split('?').length - 1;
-    const results = [] as RowDataPacket;
 
-	if (type !== null) await connection.beginTransaction();
+    if (type !== null) await connection.beginTransaction();
 
     for (let values of parameters as CFXParameters[]) {
       const executionTime = process.hrtime();
       values = parseValues(placeholders, values);
-      const [rows] = (await connection.execute(query, values)) as RowDataPacket[][];
+      const [results] = (await connection.execute(query, values)) as RowDataPacket[][];
 
-      if (rows.length > 1) {
-        for (const row of rows) {
-          results.push(parseResponse(type, row));
-        }
-      } else results.push(parseResponse(type, rows));
+      if (cb) {
+        if (results.length > 1) {
+          for (const value of results) {
+            response.push(parseResponse(type, value));
+          }
+        } else response.push(parseResponse(type, results));
+      }
 
       logQuery(invokingResource, query, process.hrtime(executionTime)[1] / 1e6, values as typeof parameters);
-    }
-
-    response = results;
-
-    if (results.length === 1) {
-      if (type === null) {
-        if (results[0][0] && Object.keys(results[0][0]).length === 1) response = Object.values(results[0][0])[0] as any;
-        else response = results[0][0];
-      } else {
-        response = results[0];
-      }
-    } else {
-      response = results;
     }
 
     if (type !== null) await connection.commit();
@@ -65,8 +53,16 @@ export const rawExecute = async (
     connection.release();
   }
 
-  if (cb)
-    try {
+  if (cb) {
+    if (response.length === 1) {
+      if (type === null) {
+        if (response[0][0] && Object.keys(response[0][0]).length === 1) cb(Object.values(response[0][0])[0] as any);
+        else cb(response[0][0]);
+      } else {
+        cb(response[0]);
+      }
+    } else {
       cb(response);
-    } catch {}
+    }
+  }
 };
