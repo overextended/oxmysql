overextended/oxmysql

commit ef0dfaac8410a529c3f315157457b03cec6c3806

refactor(execute): remove mysql2 promise api
Changing from promises to callbacks gives ~20% increased performance.
Single prepared statements won't use transactions (no extra overhead).

src/database/rawExecute.ts
@@ -1,7 +1,7 @@
-import { QueryError, ResultSetHeader, RowDataPacket } from 'mysql2';
+import { RowDataPacket } from 'mysql2';
 import { pool } from '.';
 import { logQuery } from '../logger';
-import { CFXCallback, CFXParameters, QueryResponse } from '../types';
+import { CFXCallback, CFXParameters } from '../types';
 import { parseResponse } from '../utils/parseResponse';
 import { executeType, parseExecute, parseValues } from '../utils/parseExecute';
 import { scheduleTick } from '../utils/scheduleTick';
@@ -9,60 +9,86 @@ import { scheduleTick } from '../utils/scheduleTick';
 export const rawExecute = async (
   invokingResource: string,
   query: string,
-  parameters: CFXParameters | CFXParameters[],
+  parameters: CFXParameters,
   cb?: CFXCallback
 ) => {
   const type = executeType(query);
-
   parameters = parseExecute(parameters);
+
+  let single: boolean;
+
+  if (!parameters.every(Array.isArray)) {
+    single = true;
+    parameters = [[...parameters]];
+  }
+
+  const length = parameters.length - 1;
 
   await scheduleTick();
 
-  const connection = await pool.promise().getConnection();
-  const response = [] as RowDataPacket;
+  await new Promise((resolve, reject) => {
+    pool.getConnection((err, connection) => {
+      if (err) return reject([connection, err]);
 
-  try {
-    const placeholders = query.split('?').length - 1;
+      const execute = () => {
+        const placeholders = query.split('?').length - 1;
+        const response = [] as RowDataPacket;
 
-    if (type !== null) await connection.beginTransaction();
+        parameters.forEach((values, index) => {
+          const executionTime = process.hrtime();
+          values = parseValues(placeholders, values);
 
-    for (let values of parameters as CFXParameters[]) {
-      const executionTime = process.hrtime();
-      values = parseValues(placeholders, values);
-      const [results] = (await connection.execute(query, values)) as RowDataPacket[][];
+          connection.execute(query, values, (err, results: RowDataPacket[][]) => {
+            if (err) return reject([connection, err]);
 
+            if (results.length > 1) {
+              for (const value of results) {
+                response.push(parseResponse(type, value));
+              }
+            } else response.push(parseResponse(type, results));
+
+            logQuery(invokingResource, query, process.hrtime(executionTime)[1] / 1e6, values as typeof parameters);
+
+            if (index === length) {
+              if (!single && type !== null) connection.commit();
+              connection.release();
+
+              resolve(response);
+            }
+          });
+        });
+      };
+
+      if (single || type !== null) {
+        execute();
+      } else
+        connection.beginTransaction((err) => {
+          if (err) return reject(err);
+          execute();
+        });
+    });
+  })
+    .catch(([connection, err]) => {
+      if (!single && type !== null) connection.rollback();
+
+      throw new Error(`${invokingResource} was unable to execute a query!
+		${err.message}
+		${err.sql}`);
+    })
+    // somebody who cares about types can do this
+    .then((results: any) => {
       if (cb) {
-        if (results.length > 1) {
-          for (const value of results) {
-            response.push(parseResponse(type, value));
+        if (single) {
+          if (type === null) {
+            //@ts-expect-error
+            if (results[0][0] && Object.keys(results[0][0]).length === 1) cb(Object.values(results[0][0])[0]);
+            else cb(results[0][0]);
+          } else {
+            cb(results[0]);
           }
-        } else response.push(parseResponse(type, results));
+        } else {
+          cb(results);
+        }
       }
-
-      logQuery(invokingResource, query, process.hrtime(executionTime)[1] / 1e6, values as typeof parameters);
-    }
-
-    if (type !== null) await connection.commit();
-  } catch (e) {
-    await connection.rollback();
-
-    throw new Error(`${invokingResource} was unable to execute a query!
-    ${(e as QueryError).message}
-    ${`${(e as any).sql}`}`);
-  } finally {
-    connection.release();
-  }
-
-  if (cb) {
-    if (response.length === 1) {
-      if (type === null) {
-        if (response[0][0] && Object.keys(response[0][0]).length === 1) cb(Object.values(response[0][0])[0] as any);
-        else cb(response[0][0]);
-      } else {
-        cb(response[0]);
-      }
-    } else {
-      cb(response);
-    }
-  }
+    });
 };

src/types/index.ts
@@ -12,6 +12,7 @@ export type TransactionQuery = {
   values?: CFXParameters;
 };
 
-export type CFXParameters = Record<string, unknown> | unknown[];
+// working with this type is impossible but at least we can pretend to be strictly typed
+export type CFXParameters = any[];
 
 export type CFXCallback = (result: QueryResponse | number | null) => void;

src/utils/parseExecute.ts
@@ -15,7 +15,7 @@ export const executeType = (query: string) => {
   }
 };
 
-export const parseExecute = (parameters: CFXParameters | CFXParameters[]) => {
+export const parseExecute = (parameters: CFXParameters) => {
   if (!Array.isArray(parameters)) {
     if (typeof parameters === 'object') {
       const arr: unknown[] = [];
@@ -24,12 +24,10 @@ export const parseExecute = (parameters: CFXParameters | CFXParameters[]) => {
     } else throw new Error(`Parameters expected an array but received ${typeof parameters} instead`);
   }
 
-  if (!parameters.every(Array.isArray)) parameters = [[...parameters]];
-
   return parameters;
 };
 
-export const parseValues = (placeholders: number, parameters: CFXParameters | CFXParameters[]) => {
+export const parseValues = (placeholders: number, parameters: CFXParameters) => {
   if (!Array.isArray(parameters)) {
     if (typeof parameters === 'object') {
       const arr: unknown[] = [];

commit fa1cdcc66818ffab419b2a408becd12bf9746ce0

fix(execute): incorrect response parsing
Fixes several regressions and issues from ef0dfaa.
Sending a single parameter would set 'single' to true, ignoring multiple
results from the database and returning 'null'.
SELECT should not trigger transactions, not the other way around.
Results should not be parsed when no callback is defined.

fxmanifest.lua
@@ -3,7 +3,7 @@ game 'common'
 
 name 'oxmysql'
 description 'Database wrapper for FiveM utilising node-mysql2 offering improved performance and security.'
-version '2.2.3'
+version '2.2.4'
 url 'https://github.com/overextended/oxmysql'
 author 'overextended'
 

src/database/rawExecute.ts
@@ -14,13 +14,9 @@ export const rawExecute = async (
 ) => {
   const type = executeType(query);
   parameters = parseExecute(parameters);
-
   let single: boolean;
 
-  if (!parameters.every(Array.isArray)) {
-    single = true;
-    parameters = [[...parameters]];
-  }
+  if (!parameters.every(Array.isArray)) parameters = [[...parameters]];
 
   const length = parameters.length - 1;
 
@@ -41,15 +37,18 @@ export const rawExecute = async (
           connection.execute(query, values, (err, results: RowDataPacket[][]) => {
             if (err) return reject([connection, err]);
 
-            if (results.length > 1) {
-              for (const value of results) {
-                response.push(parseResponse(type, value));
-              }
-            } else response.push(parseResponse(type, results));
+            if (cb) {
+              if (results.length > 1) {
+                for (const value of results) {
+                  response.push(parseResponse(type, value));
+                }
+              } else response.push(parseResponse(type, results));
+            }
 
             logQuery(invokingResource, query, process.hrtime(executionTime)[1] / 1e6, values as typeof parameters);
 
             if (index === length) {
+              single = response.length === 1;
               if (!single && type !== null) connection.commit();
               connection.release();
 
@@ -59,7 +58,7 @@ export const rawExecute = async (
         });
       };
 
-      if (single || type !== null) {
+      if (type === null) {
         execute();
       } else
         connection.beginTransaction((err) => {