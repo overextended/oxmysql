5d69db51da4213c1165e34bef4f3acc0ea709c49
Linden <65407488+thelindat@users.noreply.github.com>
2022-04-30 04:13:31 +1000

    refactor(database): new URI parser

--- src/config/index.ts
+++ src/config/index.ts
@@ -19,21 +19,3 @@ export const mysql_transaction_isolation_level = (() => {
       return `${query} READ COMMITTED`;
   }
 })();
-
-export const connectionOptions = (() => {
-  if (mysql_connection_string.includes('mysql://')) return { uri: mysql_connection_string };
-
-  const options = mysql_connection_string
-    .replace(/(?:host(?:name)|ip|server|data\s?source|addr(?:ess)?)=/gi, 'host=')
-    .replace(/(?:user\s?(?:id|name)?|uid)=/gi, 'user=')
-    .replace(/(?:pwd|pass)=/gi, 'password=')
-    .replace(/(?:db)=/gi, 'database=')
-    .split(';')
-    .reduce<Record<string, string>>((connectionInfo, parameter) => {
-      const [key, value] = parameter.split('=');
-      connectionInfo[key] = value;
-      return connectionInfo;
-    }, {});
-
-  return options;
-})();
