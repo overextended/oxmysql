1477812b383a4eeb028368e0575e9ed153e53161
Linden <65407488+thelindat@users.noreply.github.com>
2022-07-12 14:23:39 +1000

    refactor(lib/lua): improve definitions
    Improve return types and support callbacks.

--- lib/define.lua
+++ lib/define.lua
@@ -1,11 +1,11 @@
---- EmmyLua Annotations / VSCode Lua Language Server
---- https://marketplace.visualstudio.com/items?itemName=sumneko.lua
+---@meta
+---EmmyLua Annotations / VSCode Lua Language Server
+---https://marketplace.visualstudio.com/items?itemName=sumneko.lua
 
 
 ---@param query string
----@param parameters? table|function
----@param cb? function
----@return number affectedRows
+---@param parameters? table | fun(result: number)
+---@param cb? fun(affectedRows: number)
 function MySQL.update(query, parameters, cb) end
 MySQL.Async.execute = MySQL.update
 
@@ -15,62 +15,66 @@ MySQL.Async.execute = MySQL.update
 function MySQL.update.await(query, parameters) end
 MySQL.Sync.execute = MySQL.update.await
 
+---@class Result
+---@field affectedRows number?
+---@field fieldCount number?
+---@field info string?
+---@field insertId number?
+---@field serverStatus number?
+---@field warningStatus number?
+---@field changedRows number?
+
 ---@param query string
----@param parameters? table|function
----@param cb? function
----@return table<unknown, unknown> | nil result
+---@param parameters? table | fun(result: Result | table<number, table<string, unknown>> | nil)
+---@param cb? fun(result: Result | table<number, table<string, unknown>> | nil)
 function MySQL.query(query, parameters, cb) end
 MySQL.Async.fetchAll = MySQL.query
 
 ---@param query string
 ---@param parameters? table
----@return table<unknown, unknown> | nil result
+---@return Result | table<number, table<string, unknown>> | nil result
 function MySQL.query.await(query, parameters) end
 MySQL.Sync.fetchAll = MySQL.query.await
 
 ---@param query string
----@param parameters? table|function
----@param cb? function
----@return any column
+---@param parameters? table | fun(column: unknown | nil)
+---@param cb? fun(column: unknown | nil)
 function MySQL.scalar(query, parameters, cb) end
 MySQL.Async.fetchScalar = MySQL.scalar
 
 ---@param query string
 ---@param parameters? table
----@return any column
+---@return unknown | nil column
 function MySQL.scalar.await(query, parameters) end
 MySQL.Sync.fetchScalar = MySQL.scalar.await
 
 ---@param query string
----@param parameters? table|function
----@param cb? function
----@return table<string, any> row
+---@param parameters? table | fun(row: table<string, unknown> | nil)
+---@param cb? fun(row: table<string, unknown> | nil)
 function MySQL.single(query, parameters, cb) end
 MySQL.Async.fetchSingle = MySQL.single
 
 ---@param query string
 ---@param parameters? table
----@return table<string, any> row
+---@return table<string, unknown> | nil row
 function MySQL.single.await(query, parameters) end
 MySQL.Sync.fetchSingle = MySQL.single.await
 
 ---@param query string
----@param parameters? table|function
----@param cb? function
----@return number insertId
+---@param parameters? table | fun(insertId: number)
+---@param cb? fun(insertId: number)
 function MySQL.insert(query, parameters, cb) end
 MySQL.Async.insert = MySQL.insert
 
 ---@param query string
 ---@param parameters? table
 ---@return number insertId
-function MySQL.insert.async(query, parameters) end
+function MySQL.insert.await(query, parameters) end
 MySQL.Sync.insert = MySQL.insert.await
 
 ---@param queries table
----@param parameters? table|function
----@param cb? function
----@return boolean success
+---@param parameters? table | fun(success: boolean)
+---@param cb? fun(success: boolean)
 function MySQL.transaction(queries, parameters, cb) end
 MySQL.Async.transaction = MySQL.transaction
 
@@ -82,13 +86,12 @@ MySQL.Sync.transaction = MySQL.transaction.await
 
 ---@param query string
 ---@param parameters table
----@param cb? function
----@return unknown result
+---@param cb? fun(result: unknown | nil)
 function MySQL.prepare(query, parameters, cb) end
 MySQL.Async.prepare = MySQL.prepare
 
 ---@param query string
 ---@param parameters table
----@return unknown result
+---@return unknown | nil result
 function MySQL.prepare.await(query, parameters) end
 MySQL.Sync.prepare = MySQL.prepare.await
