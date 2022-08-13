521aa8f03df7697200a96329aed51a9023d2f58b
Linden <65407488+thelindat@users.noreply.github.com>
2022-08-13 16:22:45 +1000

    refactor: move connectionOptions to config
    We'll also ensure namedPlaceholders is boolean and export the object.

--- src/config/index.ts
+++ src/config/index.ts
@@ -27,6 +27,54 @@ export const mysql_transaction_isolation_level = (() => {
   }
 })();
 
+const parseUri = (connectionString: string) => {
+  const splitMatchGroups = connectionString.match(
+    new RegExp(
+      '^(?:([^:/?#.]+):)?(?://(?:([^/?#]*)@)?([\\w\\d\\-\\u0100-\\uffff.%]*)(?::([0-9]+))?)?([^?#]+)?(?:\\?([^#]*))?$'
+    )
+  ) as RegExpMatchArray;
+
+  if (!splitMatchGroups) throw new Error(`mysql_connection_string structure was invalid (${connectionString})`);
+
+  const authTarget = splitMatchGroups[2] ? splitMatchGroups[2].split(':') : [];
+
+  const options = {
+    user: authTarget[0] || undefined,
+    password: authTarget[1] || undefined,
+    host: splitMatchGroups[3],
+    port: parseInt(splitMatchGroups[4]),
+    database: splitMatchGroups[5].replace(/^\/+/, ''),
+    ...(splitMatchGroups[6] &&
+      splitMatchGroups[6].split('&').reduce<Record<string, string>>((connectionInfo, parameter) => {
+        const [key, value] = parameter.split('=');
+        connectionInfo[key] = value;
+        return connectionInfo;
+      }, {})),
+  };
+
+  return options;
+};
+
+export const connectionOptions = (() => {
+  const options: Record<string, any> = mysql_connection_string.includes('mysql://')
+    ? parseUri(mysql_connection_string)
+    : mysql_connection_string
+        .replace(/(?:host(?:name)|ip|server|data\s?source|addr(?:ess)?)=/gi, 'host=')
+        .replace(/(?:user\s?(?:id|name)?|uid)=/gi, 'user=')
+        .replace(/(?:pwd|pass)=/gi, 'password=')
+        .replace(/(?:db)=/gi, 'database=')
+        .split(';')
+        .reduce<Record<string, string>>((connectionInfo, parameter) => {
+          const [key, value] = parameter.split('=');
+          connectionInfo[key] = value;
+          return connectionInfo;
+        }, {});
+
+  options.namedPlaceholders = options.namedPlaceholders === 'false' ? false : true;
+
+  return options;
+})();
+
 RegisterCommand(
   'oxmysql_debug',
   (source: number, args: string[]) => {
