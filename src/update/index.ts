77f0d32e0783c9d81133a1f4100d6d2bca4642f4
Linden <65407488+thelindat@users.noreply.github.com>
2022-08-23 20:14:58 +1000

    fix(update): correct semver comparison

--- src/update/index.ts
+++ src/update/index.ts
@@ -5,29 +5,21 @@ if (GetConvar('mysql_versioncheck', 'true') === 'true') {
   setTimeout(async () => {
     try {
       const response = await fetch(`https://api.github.com/repos/overextended/oxmysql/releases/latest`);
-
       if (response.status !== 200) return;
 
       const release = (await response.json()) as any;
       if (release.prerelease) return;
 
-      const currentVersion = GetResourceMetadata(resourceName, 'version', 0).match(/(\d)\.(\d+)\.(\d+)/);
+      const currentVersion = GetResourceMetadata(resourceName, 'version', 0).match(/(\d)\.(\d+\.\d+)/);
       if (!currentVersion) return;
 
-      const latestVersion = release.tag_name.match(/(\d)\.(\d+)\.(\d+)/);
+      const latestVersion = release.tag_name.match(/(\d)\.(\d+\.\d+)/);
       if (!latestVersion) return;
 
-      if (currentVersion[0] === latestVersion[0] || currentVersion[0] > latestVersion[0]) return;
-
-      const updateMessage =
-        currentVersion[3] < latestVersion[3]
-          ? 'patch'
-          : currentVersion[2] < latestVersion[2]
-          ? 'an update'
-          : 'a major update';
+      if (currentVersion[0] === latestVersion[0] || parseInt(currentVersion[1]) > parseInt(latestVersion[1]) || parseFloat(currentVersion[2]) > parseFloat(latestVersion[2])) return;
 
       console.log(
-        `^3There is ${updateMessage} available for oxmysql - please update to the latest release (current version: ${currentVersion[0]})\r\n${release.html_url}^0`
+        `^3An update is available for oxmysql (current version: ${currentVersion[0]})\r\n${release.html_url}^0`
       );
     } catch (e) {}
   }, 1000);
