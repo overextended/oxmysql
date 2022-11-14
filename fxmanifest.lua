30e37af313d3cd2ad7fa1d1f8d3ef32662dd8904
Manifest Bumper <41898282+github-actions[bot]@users.noreply.github.com>
2022-11-14 01:42:33 +0000

    chore: bump manifest version to v2.5.3

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'common'
 
 name 'oxmysql'
 description 'Database wrapper for FXServer utilising node-mysql2 offering improved performance and security.'
-version '2.5.2'
+version '2.5.3'
 url 'https://github.com/overextended/oxmysql'
 author 'overextended'
 
