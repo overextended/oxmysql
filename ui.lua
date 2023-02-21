21c63060567c68dc898233fc63f910ab3f8a5179
Vinicius7199 <viniciusdeandradefernandes5@gmail.com>
2023-02-21 12:42:07 -0300

    oxmysql UI apenas para Admin

--- ui.lua
+++ ui.lua
@@ -1,14 +1,24 @@
+TriggerEvent("dw_morecontrols:getControls", function(cb)
+    MC = cb
+end)
+
 RegisterNetEvent('oxmysql:openUi', function(data)
+    if not LocalPlayer.state.isAdmin then
+        return false
+    end
+    
     SendNUIMessage({
         action = 'openUI',
         data = data
     })
     SetNuiFocus(true, true)
+    MC.StartFocus(GetCurrentResourceName())
 end)
 
 RegisterNUICallback('exit', function(_, cb)
     cb(true)
     SetNuiFocus(false, false)
+    MC.EndFocus(GetCurrentResourceName())
 end)
 
 RegisterNUICallback('fetchResource', function(data, cb)
