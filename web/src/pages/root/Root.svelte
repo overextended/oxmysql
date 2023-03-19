754d4ac7a1f15b7250668c0af249f2d012fd4219
Luke <39926192+LukeWasTakenn@users.noreply.github.com>
2023-03-19 13:26:34 +0100

    refactor(web): move search into header and add icons

--- web/src/pages/root/Root.svelte
+++ web/src/pages/root/Root.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
   import Input from '../../components/Input.svelte';
-  import { IconSearch } from '@tabler/icons-svelte';
+  import { IconFileAnalytics, IconSearch, IconSourceCode } from '@tabler/icons-svelte';
   import { router } from 'tinro';
   import { search, filteredResources, generalData } from '../../store';
 </script>
 
-<div class="p-4 w-full h-full flex justify-between gap-4">
+<div class="p-2 w-full h-full flex justify-between gap-2">
   <div class="bg-dark-700 p-4 pr-0 flex flex-col w-2/3 rounded-md">
-    <div class="pr-4">
-      <p class="text-2xl mb-4">Resources</p>
+    <div class="pr-4 flex justify-between items-center">
+      <div class="flex gap-3 items-center ">
+        <p class="text-2xl">Resources</p>
+        <IconSourceCode />
+      </div>
       <Input icon={IconSearch} bind:value={$search} />
     </div>
     <div class="flex flex-col gap-3 mt-6 overflow-y-auto pr-4">
@@ -23,7 +26,10 @@
     </div>
   </div>
   <div class="bg-dark-700 p-4 flex flex-col w-1/3 rounded-md">
-    <p class="text-2xl mb-4">General data</p>
+    <div class="flex gap-3 items-center mb-4">
+      <p class="text-2xl">General data</p>
+      <IconFileAnalytics />
+    </div>
     <div class="flex flex-col text-dark-50">
       <p>Queries: {$generalData.queries}</p>
       <p>Time querying: {$generalData.timeQuerying} ms</p>
