6a014b51e2b2b85a71a7cecba73782d8295e2bf4
Linden <65407488+thelindat@users.noreply.github.com>
2024-03-05 01:05:32 +1100

    refactor(db/startTransaction): remove execute from cb

--- src/database/startTransaction.ts
+++ src/database/startTransaction.ts
@@ -1,6 +1,7 @@
 import { getPoolConnection } from './connection';
 import { logError } from '../logger';
 import { CFXCallback, CFXParameters } from '../types';
+import { parseArguments } from 'utils/parseArguments';
 
 export const startTransaction = async (
   invokingResource: string,
@@ -17,13 +18,10 @@ export const startTransaction = async (
   try {
     await conn.beginTransaction();
 
-    const commit = await queries({
-      query: (sql: string, values: CFXParameters) => {
-        return conn.query(sql, values);
-      },
-      execute: (sql: string, values: CFXParameters) => {
-        return conn.execute(sql, values);
-      },
+    const commit = await queries(async (sql: string, values: CFXParameters) => {
+      [sql, values] = parseArguments(sql, values);
+      const [rows] = await conn.query(sql, values);
+      return rows;
     });
 
     response = commit === false ? false : true;
