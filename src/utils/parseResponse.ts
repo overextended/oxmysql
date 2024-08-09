bb6fea76904eb0688d9c78ddb04a93806a89f310
Linden <65407488+thelindat@users.noreply.github.com>
2024-08-09 15:22:13 +1000

    fix(utils/parseResponse): use nullish coalescing operator

--- src/utils/parseResponse.ts
+++ src/utils/parseResponse.ts
@@ -4,19 +4,19 @@ import type { QueryResponse, QueryType } from '../types';
 export const parseResponse = (type: QueryType, result: QueryResponse): any => {
   switch (type) {
     case 'insert':
-      return (result as ResultSetHeader)?.insertId || null;
+      return (result as ResultSetHeader)?.insertId ?? null;
 
     case 'update':
-      return (result as ResultSetHeader)?.affectedRows || null;
+      return (result as ResultSetHeader)?.affectedRows ?? null;
 
     case 'single':
-      return (result as RowDataPacket[])?.[0] || null;
+      return (result as RowDataPacket[])?.[0] ?? null;
 
     case 'scalar':
       const row = (result as RowDataPacket[])?.[0];
-      return (row && Object.values(row)[0]) || null;
+      return (row && Object.values(row)[0]) ?? null;
 
     default:
-      return result || null;
+      return result ?? null;
   }
 };
